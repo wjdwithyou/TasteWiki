bff685d84dc75b1bb9aceed4cfa68efa843dbc8d
wjdwithyou <asdf142857@gmail.com>
2017-08-04 15:34:17 +0900

    fix css about buttons in community_detail

--- resources/views/community_detail.php
+++ resources/views/community_detail.php
@@ -24,8 +24,8 @@
 						<h1><?=$data->title?></h1>
 						<?php if ($logined && $data->account_idx == $_SESSION['idx']) :?>
 							<p align="right">
-								<button type="button" class="cm_btn" onclick="iModifyComm()">수정</button>
-								<button type="button" class="cm_btn" onclick="deleteComm">삭제</button>
+								<button type="button" class="tw_btn48" onclick="iModifyComm()">수정</button>
+								<button type="button" class="tw_btn48" onclick="deleteComm">삭제</button>
 							</p>
 						<?php endif;?>
 						<p align="right">작성자: <?=$data->nickname?><br/>마지막 수정: <?=$data->lastdate?><br/>hit: <?=$data->hit_cnt?></p>
@@ -43,12 +43,12 @@
 									<td class="reply_write_td" width="50px"><img src="<?=$adr_s3?>profile/<?=$_SESSION['img']?>" height="20" width="20"/></td>
 									<td class="reply_write_td" width="150px"><a onclick=""><?=$_SESSION['nickname']?></a></td>
 									<td class="reply_write_td" width="600px"><textarea id="cm_reply_content" class="cm_reply_content" rows="3"></textarea></td>
-									<td class="reply_write_td" width="150px"><button type="button" onclick="writeReply();">등록</button></td>
+									<td class="reply_write_td" width="150px"><button type="button" class="tw_btn48" onclick="writeReply();">등록</button></td>
 								<?php else :?>
 									<td class="reply_write_td" width="50px"><img src="<?=$adr_s3?>profile/default.png" height="20" width="20"/></td>
 									<td class="reply_write_td" width="150px">GUEST</td>
 									<td class="reply_write_td" width="600px"><textarea class="cm_reply_content" rows="3" placeholder="로그인 후에 이용해주세요." rows="3" disabled></textarea></td>
-									<td class="reply_write_td" width="150px"><button type="button" onclick="alert('로그인 후에 이용해주세요.');">등록</button></td>
+									<td class="reply_write_td" width="150px"><button type="button" class="tw_btn48" onclick="alert('로그인 후에 이용해주세요.');">등록</button></td>
 								<?php endif;?>
 								</tr>
 							</table>
